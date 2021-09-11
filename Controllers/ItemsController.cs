767574d2c5335c8b9c4f0095c0a65e0e72a33c33
rootslowed <rootslowed@gmail.com>
2021-09-11 19:18:37 -0300

    item controller with get items and get item

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ItemsController : ControllerBase
+  {
+    private readonly InMemItemsRepository _repository;
+
+    public ItemsController()
+    {
+      _repository = new InMemItemsRepository();
+    }
+
+    [HttpGet]
+    public IEnumerable<Item> GetItems()
+    {
+      var items = _repository.GetItems();
+      return items;
+    }
+
+    [HttpGet("{id:guid}")]
+    public ActionResult<Item> GetItem(Guid id)
+    {
+      var item = _repository.GetItem(id);
+      
+      if (item is null)
+      {
+        return NotFound();
+      }
+      
+      return item;
+    }
+  }
+}
