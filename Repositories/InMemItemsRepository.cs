230393f0da889267b4a3b94ffa86dfc1eb7dc2ec
rootslowed <rootslowed@gmail.com>
2021-09-11 19:18:57 -0300

    in memory repository to test

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+  public class InMemItemsRepository
+  {
+    private readonly List<Item> items = new()
+    {
+      new Item {Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow},
+      new Item {Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow},
+      new Item {Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow}
+    };
+
+    public IEnumerable<Item> GetItems()
+    {
+      return items;
+    }
+
+    public Item GetItem(Guid id)
+    {
+      return items.SingleOrDefault(item => item.Id == id);
+    }
+  }
+}
