0f772031b40a1cf3324c667827bbf2ff2aacd6e7
rootslowed <rootslowed@gmail.com>
2021-09-11 19:42:14 -0300

    interface InMemItemsRepository to implement dependency injection

---
+++ Repositories/IInMemItemsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+  public interface IInMemItemsRepository
+  {
+    Item GetItem(Guid id);
+    IEnumerable<Item> GetItems();
+  }
+}
