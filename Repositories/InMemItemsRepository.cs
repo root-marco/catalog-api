a61e50fd322770da4afe63347062d1cfe3497d1d
rootslowed <rootslowed@gmail.com>
2021-09-11 19:42:45 -0300

    class implements interface IInMemItems

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -5,7 +5,7 @@ using Catalog.Entities;
 
 namespace Catalog.Repositories
 {
-  public class InMemItemsRepository
+  public class InMemItemsRepository : IInMemItemsRepository
   {
     private readonly List<Item> items = new()
     {
