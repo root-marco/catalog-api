97940ba09aa361bfdacc6d388ee064760a213668
rootslowed <rootslowed@gmail.com>
2021-09-12 15:13:24 -0300

    logic to save the updated item to a "database"

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -28,5 +28,11 @@ namespace Catalog.Repositories
     {
       items.Add(item);
     }
+
+    public void UpdateItem(Item item)
+    {
+      var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+      items[index] = item;
+    }
   }
 }
