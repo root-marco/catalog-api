a84bd3664364946e49fc201645438dd919a3881a
rootslowed <rootslowed@gmail.com>
2021-09-12 15:12:17 -0300

    some validations on name and price

--- Dtos/CreateItemDto.cs
+++ Dtos/CreateItemDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Catalog.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Dtos
 {
   public record CreateItemDto
   {
+    [Required]
     public string Name { get; init; }
+    [Required]
+    [Range(0, 9999)]
     public decimal Price { get; init; }
   }
 }
