2a668d17cdcb57484c58499d592837bf3569acae
rootslowed <rootslowed@gmail.com>
2021-09-13 16:32:32 -0300

    user and password fields

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -1,10 +1,12 @@
-﻿namespace Catalog.Settings
+﻿namespace Catalog.Api.Settings
 {
   public class MongoDbSettings
   {
     public string Host { get; set; }
     public int Port { get; set; }
+    public string User { get; set; }
+    public string Password { get; set; }
 
-    public string ConnectionString => $"mongodb://{Host}:{Port}";
+    public string ConnectionString => $"mongodb://{User}:{Password}@{Host}:{Port}";
   }
 }
