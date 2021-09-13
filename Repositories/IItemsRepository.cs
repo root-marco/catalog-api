5eeeaf94a1f2a9ff43408ae81b7def3ad271a40d
rootslowed <rootslowed@gmail.com>
2021-09-13 11:53:54 -0300

    type of return changed to tasks

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Catalog.Entities;
 
 namespace Catalog.Repositories
 {
   public interface IItemsRepository
   {
-    Item GetItem(Guid id);
-    IEnumerable<Item> GetItems();
-    void CreateItem(Item item);
-    void UpdateItem(Item item);
-    void DeleteItem(Guid id);
+    Task<Item> GetItemAsync(Guid id);
+    Task<IEnumerable<Item>> GetItemsAsync();
+    Task CreateItemAsync(Item item);
+    Task UpdateItemAsync(Item item);
+    Task DeleteItemAsync(Guid id);
   }
 }
