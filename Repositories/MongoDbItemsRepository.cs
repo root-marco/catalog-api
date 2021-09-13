a8648af7b50a166cc338976fd1bd6049944ea685
rootslowed <rootslowed@gmail.com>
2021-09-13 08:58:45 -0300

    initial config file

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+  public class MongoDbItemsRepository : IItemsRepository
+  {
+    private const string databaseName = "catalog";
+    private const string collectionName = "items";
+    private readonly IMongoCollection<Item> itemsCollection;
+    
+    public MongoDbItemsRepository(IMongoClient mongoClient)
+    {
+      var database = mongoClient.GetDatabase(databaseName);
+      itemsCollection = database.GetCollection<Item>(collectionName);
+    }
+    
+    public Item GetItem(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IEnumerable<Item> GetItems()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void CreateItem(Item item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void UpdateItem(Item item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void DeleteItem(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
