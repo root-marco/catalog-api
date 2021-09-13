75e263443c80c138fd5cdc5e0d41b64befd28e79
rootslowed <rootslowed@gmail.com>
2021-09-13 11:55:01 -0300

    mongodb settings with host, port and connection string

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Settings
+{
+  public class MongoDbSettings
+  {
+    public string Host { get; set; }
+    public int Port { get; set; }
+
+    public string ConnectionString => $"mongodb://{Host}:{Port}";
+  }
+}
